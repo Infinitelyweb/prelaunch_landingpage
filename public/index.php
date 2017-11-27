79608fdfa958d5b704c0076544c765b6604406b2
sarahswoboda <sarahswoboda@192.168.192.40>
2017-11-27 23:45:37 +0100

    update index and thankyou page

--- public/index.php
+++ public/index.php
@@ -1,14 +1,9 @@
 <?php 
-require_once __DIR__.'/config/autoload.php';
+require_once __DIR__.'/../config/autoload.php';
 
 use Infinitely\Page;
 
-$page = new Page();
-
-// override default settings
-$header = [
-    'brand' => 'My Landing Page NEW',
-];
-$page->addPageItems('header', $header);
+// get page with default values
+$page = new Page('homepage');
 
 echo $twig->render('view/landingpage-nonav.html.twig', $page->getPageItems());
--- public/thankyou.php
+++ public/thankyou.php
@@ -1,15 +1,9 @@
 <?php 
-require_once __DIR__.'/config/autoload.php';
+require_once __DIR__.'/../config/autoload.php';
 
 use Infinitely\Page;
 
-$page = new Page();
-
-// override default settings
-$masterhead = [
-    'headline' => 'Thank you!',
-    'lead' => 'please check your emails!'
-];
-$page->addPageItems('masterhead', $masterhead);
+// get page with default values
+$page = new Page('masterhead');
 
 echo $twig->render('view/thankyou.html.twig', $page->getPageItems());
